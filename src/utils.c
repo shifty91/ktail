4b101e551f4b06b9f2a47c7f73b3da8366566a8a
Kurt Kanzenbach <kurt@kmk-computers.de>
2016-10-03 17:49:32 +0200

    src: added license header

--- src/utils.c
+++ src/utils.c
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2016 Kurt Kanzenbach <kurt@kmk-computers.de>
+ *
+ * This file is part of Ktail.
+ *
+ * Ktail is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Ktail is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Ktail.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <stdlib.h>
 #include <limits.h>
 #include <errno.h>
